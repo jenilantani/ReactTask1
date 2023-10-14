3f93e94ff24cfb16f3955699616452b5cdae8b11
Your Name <you@example.com>
2023-10-14 21:36:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (event) => {
+    const newText = event.target.value;
+    setText(newText);
+  };
+
+  const wordCount = text.split(/\s+/).filter((word) => word !== '').length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{ textAlign: 'center' }}>
+      <h1>Responsive Paragraph Word Counter</h1>
+      <div style={{ display: 'inline-block', textAlign: 'left' }}>
+        <textarea
+          rows="5"
+          placeholder="Write your paragraph here..."
+          value={text}
+          onChange={handleTextChange}
+        />
+        <p style={{ color: 'grey' }}>Word count: {wordCount}</p>
+      </div>
     </div>
   );
 }
